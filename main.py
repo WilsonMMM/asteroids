b8b6d54dbd3a6371584bf0a927f5722e265798d5
MW <m.wilson.www@gmail.com>
2025-01-25 09:12:11 +0000

    Swapped to using containers for update and draw calls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -8,7 +9,14 @@ def main():
 
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,8 +24,13 @@ def main():
                 return
 
         screen.fill("#000000")
+        for s in updatable:
+            s.update(dt)
+        for s in drawable:
+            s.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
